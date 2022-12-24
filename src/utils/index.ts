93ea380d1fa30ec84060c4f94cc588b95e30da58
Prince Muel <vansomecsam@gmail.com>
2022-12-24 04:00:41 +0100

    feat: add base utils
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/utils/index.ts
@@ -0,0 +1,84 @@
+type ClassValue =
+  | ClassArray
+  | ClassDictionary
+  | string
+  | number
+  | null
+  | boolean
+  | undefined;
+type ClassDictionary = Record<string, any>;
+type ClassArray = ClassValue[];
+
+const filterBoolean = <T>(arg: T) => Boolean(arg) && typeof arg !== 'boolean';
+/**
+ * This function returns classes based on conditions
+ * that evaluate to true. It removes values that
+ * evaluate to literal booleans, false, null and undefined.
+ * @param args (string | boolean | null | undefined)[]
+ * @returns string
+ * @example clsx('base', undefined, ['more', 'classes'], hasError && 'bg-red', isEnabled || 'pointer-events-none', isTitle ? 'font-semibold' : 'font-normal')
+ * @returns "base more classes bg-red font-normal"
+ */
+// (arg) => Boolean(arg) && typeof arg !== 'boolean'
+export function clsx(...args: ClassValue[]) {
+  return args.flat().filter(filterBoolean).join(' ').trim();
+}
+
+export const capitalize = (string: string) => {
+  return string?.charAt(0)?.toUpperCase() + string?.slice(1);
+};
+
+export const trim = (string: string) => string?.trim();
+
+export function serialize<T>(data: T): T {
+  return JSON.parse(JSON.stringify(data));
+}
+
+export const removeFirstChar = (string: string) => {
+  return string?.slice(1);
+};
+
+export const isNotEmptyArray = <T>(
+  array: T[] | undefined
+): array is NonNullable<T[]> => {
+  return (array || []).length > 0;
+};
+
+export const shortName = (name: string) => {
+  const lastIndexOfSpace = name?.indexOf(' ');
+  return name?.substring(0, lastIndexOfSpace);
+};
+
+export const compose =
+  (...fns: any[]) =>
+  (res: any) =>
+    fns.reduce((accum, next) => next(accum), res);
+
+export function pluck<I, K extends keyof I>(items: I[], key: K): I[K][] {
+  return items.map((item) => item[key]);
+}
+
+/**
+ * A Generic Ranking Algorithm
+ * @param items T
+ * @param rank 'asc' | 'desc'
+ * @returns An array containing the sorted items according to the ranking algorithm
+ */
+
+export const ranker = <T>(
+  items: T[],
+  rank: 'asc' | 'desc',
+  callbackfn: (value: T) => number
+): T[] => {
+  return items
+    .map((item) => ({
+      item,
+      rank: callbackfn(item),
+    }))
+    .sort((a, b) => (rank === 'asc' ? a.rank - b.rank : b.rank - a.rank))
+    .map((ranked) => ranked.item);
+};
+
+export const clean = (data: string) => {
+  return data?.replace(/[|&;$%@"<>()\\+,]/g, '');
+};

1ab1a56b03651d8ce7e0cccaf892a3ed6b88ff79
Prince Muel <vansomecsam@gmail.com>
2022-12-24 05:30:13 +0100

    feat(ui): add header component to layout
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from './header';
 
 type Props = {};
 
 const Layout = (props: Props) => {
-  return <React.Fragment>Layout</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Header />
+      <Outlet />
+    </React.Fragment>
+  );
 };
 
 export { Layout };

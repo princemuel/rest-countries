fede3fd1bcadaed6dc25bf58455d800c3e88e35b
Prince Muel <vansomecsam@gmail.com>
2022-12-25 05:53:16 +0100

    feat: add data loaders and update routing setup
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,33 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './components';
 import { ThemeProvider } from './context';
-import { Country, Root } from './routes';
+import { Country, countryLoader, ErrorPage, Root, rootLoader } from './routes';
 import './styles/main.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route index element={<Root />} />
-
-      <Route path='countries'>
-        <Route path=':id' element={<Country />} />
-        <Route path='logout' />
-      </Route>
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Root />,
+        loader: rootLoader,
+      },
+      {
+        path: 'countries/:countryId',
+        element: <Country />,
+        loader: countryLoader,
+      },
+    ],
+  },
+]);
 
 root.render(
   <React.StrictMode>

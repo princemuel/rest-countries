95306c2d81149879ad33e8e818a12ec4a5053050
Prince Muel <vansomecsam@gmail.com>
2022-12-25 05:55:55 +0100

    refactor: update header styles
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className='shadow-header'>
-      <div className='flex items-center justify-between py-8 h-container'>
-        <Text as='h1' className='text-4xl font-extrabold text-primary-300'>
+    <header className='bg-neutral-100 shadow-header dark:bg-primary-500'>
+      <div className='flex items-center justify-between py-12 h-container'>
+        <Text as='h1' className='text-[1.6rem] font-extrabold md:text-4xl'>
           Where in the world?
         </Text>
         <ThemeBtn classname='' />

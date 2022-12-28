26532ecc42c15356f20fba9bfcbb0ca4a87a03f9
Prince Muel <vansomecsam@gmail.com>
2022-12-28 19:40:12 +0100

    chore: clean up country route
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/routes/country.tsx
+++ src/routes/country.tsx
@@ -1,15 +1,46 @@
+import { useLoaderData } from 'react-router-dom';
+import { ICountry } from '../@types';
+import { CountryTemplate } from '../components';
+import { cachedFetch } from '../lib';
+import { hasValues } from '../utils';
+
 type Props = {};
 
 const Country = (props: Props) => {
+  const { country, borders } = useLoaderData() as {
+    country: ICountry;
+    borders: {
+      name: string;
+      code: string;
+    }[];
+  };
+
   return (
     <main className='text-[1.6rem]'>
-      <section className='h-container'></section>
+      <CountryTemplate country={country} borders={borders} />
     </main>
   );
 };
 
 export { Country };
+export { loader as countryLoader };
 
-export function countryLoader() {
-  return;
+// @ts-expect-error
+async function loader({ params }) {
+  const response = await cachedFetch(`/alpha/${params?.countryId}`);
+  const country = response.data as ICountry;
+  const codes = country.borders;
+  const borders = hasValues(codes)
+    ? await Promise.all(
+        codes.map(async (code) => {
+          const response = await cachedFetch(`/alpha/${code}`);
+          const data = response.data as ICountry;
+          return { name: data.name, code: data.alpha3Code };
+        })
+      )
+    : [];
+  return {
+    country,
+    borders,
+  };
 }

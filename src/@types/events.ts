188a88aa9f0753c45e26ad4e1cdaf31c6ec966a1
Prince Muel <vansomecsam@gmail.com>
2022-12-28 19:14:55 +0100

    refactor: rename types folder to @types
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/@types/events.ts
+++ src/@types/events.ts
@@ -3,6 +3,6 @@
  *===============================*
 */
 export type ReactFormEvent = React.FormEvent<HTMLFormElement>;
-export type ReactSelectEvent = React.ChangeEvent<HTMLSelectElement>;
+export type ReactSelectEvent = React.MouseEvent<HTMLLIElement>;
 export type ReactInputEvent = React.ChangeEvent<HTMLInputElement>;
 export type ReactMouseEvent = React.MouseEvent<HTMLButtonElement>;
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export * from './use-click-outside';
 export * from './use-debounce';
 export * from './use-debouncer';
 export * from './use-timeout-fn';

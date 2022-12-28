584a53a218bc93e7f91b6c1341be735a13430829
Prince Muel <vansomecsam@gmail.com>
2022-12-28 19:17:02 +0100

    feat(ui): add sticky styles to header on scroll
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className='bg-neutral-100 shadow-header dark:bg-primary-500'>
+    <header className='sticky top-0 z-50 bg-neutral-100 shadow-header dark:bg-primary-500'>
       <div className='flex items-center justify-between py-12 h-container'>
         <Text as='h1' className='text-[1.6rem] font-extrabold md:text-4xl'>
           Where in the world?

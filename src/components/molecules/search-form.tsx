d431a945e4a72355555a385403f66216caf528cd
Prince Muel <vansomecsam@gmail.com>
2022-12-28 06:50:49 +0100

    feat(ui): add search country form
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/components/molecules/search-form.tsx
@@ -0,0 +1,37 @@
+import { useCountriesDispatch, useTheme } from '../../context';
+import { ReactInputEvent } from '../../types';
+
+type Props = {};
+
+const SearchForm = (props: Props) => {
+  const theme = useTheme();
+  const url = theme === 'light' ? 'light' : 'dark';
+  const dispatch = useCountriesDispatch();
+
+  function handleSearch(e: ReactInputEvent) {
+    dispatch({ type: 'SEARCH', payload: e.target.value });
+  }
+
+  return (
+    <form
+      role='search'
+      className='flex w-full max-w-3xl items-center gap-6 rounded-lg bg-neutral-100 px-12 py-6 shadow-input dark:bg-primary-500'
+    >
+      <label htmlFor='search'>
+        <img src={`/assets/search-${url}.svg`} aria-hidden='true' alt='back' />
+        <span className='sr-only'>Search for a country</span>
+      </label>
+
+      <input
+        type='text'
+        id='search'
+        className='w-full border-none outline-none'
+        aria-label='Search for a country…'
+        placeholder='Search for a country…'
+        onChange={handleSearch}
+      />
+    </form>
+  );
+};
+
+export { SearchForm };

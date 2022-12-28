1aff1ba6fe7a58584b48b7391823ecd17a942a88
Prince Muel <vansomecsam@gmail.com>
2022-12-28 19:23:21 +0100

    peaf: optimize search input experience
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/organisms/countries.tsx
+++ src/components/organisms/countries.tsx
@@ -1,5 +1,4 @@
 import { useCountriesState } from '../../context';
-import { hasValues } from '../../utils';
 import { Card } from '../molecules';
 
 type Props = {};
@@ -7,25 +6,30 @@ type Props = {};
 const CountriesList = (props: Props) => {
   const state = useCountriesState();
 
+  const isSearching = Boolean(state.search);
+
   const filteredCountries =
     state?.filter !== 'All'
       ? state?.countries?.filter((country) =>
           new RegExp(`${state?.filter}`, 'ig').test(country.region)
         )
-      : state?.search
-      ? state?.countries?.filter((country) =>
-          new RegExp(`${state?.search}`, 'ig').test(country.name)
-        )
       : state?.countries;
 
   return (
     <div className='grid grid-cols-[repeat(auto-fit,minmax(25rem,1fr))] justify-items-center gap-24'>
-      {hasValues(filteredCountries) &&
-        filteredCountries.map((country) => (
-          <Card key={country?.alpha3Code} country={country} />
-        ))}
+      {isSearching
+        ? filteredCountries
+            .filter((country) =>
+              new RegExp(`${state?.search}`, 'ig').test(country.name)
+            )
+            .map((country) => (
+              <Card key={country?.alpha3Code} country={country} />
+            ))
+        : filteredCountries.map((country) => (
+            <Card key={country?.alpha3Code} country={country} />
+          ))}
     </div>
   );
 };
 
-export default CountriesList;
+export { CountriesList };

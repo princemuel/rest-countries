6b437145f1f428b427b6f195a75fcce66b43ac42
Prince Muel <vansomecsam@gmail.com>
2022-12-28 06:58:02 +0100

    feat: add data fetching functions
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/lib/lib.ts
@@ -0,0 +1,19 @@
+import { cachedFetch } from './cached-fetch';
+import { ICountry } from './models';
+
+export async function getCountries() {
+  const response = await cachedFetch(
+    '/all?fields=name,population,region,capital,alpha3Code,flags'
+  );
+  return response.data as ICountry[];
+}
+export async function getRegions() {
+  const response = await cachedFetch('/all?fields=region');
+  const data = (response?.data as RegionData[]).map(({ region }) => region);
+  return [...new Set(data)];
+}
+
+type RegionData = {
+  region: string;
+  independent: boolean;
+};

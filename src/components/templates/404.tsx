64d3c18090399c4738f56662393512f9c21dae8d
Prince Muel <vansomecsam@gmail.com>
2022-12-28 07:06:32 +0100

    feat(ui): add error page template
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/components/templates/404.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import { Text } from '../atoms';
+
+type Props = {};
+
+const ErrorPageTemplate = (props: Props) => {
+  return (
+    <div>
+      <div>
+        <Text as='h1'>404</Text>
+        <p>
+          {/* @ts-expect-error */}
+          <i>{error?.statusText || error?.message}</i>
+        </p>
+        <Text>Page not found!</Text>
+        <Text>
+          This page is away on a vacation to Hawaii enjoying a coconut drink on
+          a sunny beach so you may not get it today.
+        </Text>
+        <Text>
+          Not to worry. You can go back to the
+          <Link to='/'>Homepage</Link>
+          &nbsp;or another page using the navigation menu
+        </Text>
+        <Text as='strong'>
+          (It will go back to the previous page after 10 seconds)
+        </Text>
+      </div>
+    </div>
+  );
+};
+
+export { ErrorPageTemplate };

11ad79d3c8b171919cbb3a5dc2a8fdb1fa0f5132
Prince Muel <vansomecsam@gmail.com>
2022-12-28 07:06:55 +0100

    chore(ui): move error page template from route file
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
-import { Link, useNavigate, useRouteError } from 'react-router-dom';
-import { Text } from '../components';
+import { useNavigate, useRouteError } from 'react-router-dom';
+import { ErrorPageTemplate } from '../components';
 
 type Props = {};
 
@@ -21,28 +21,7 @@ const ErrorPage = (props: Props) => {
 
   return (
     <main>
-      <div>
-        <div>
-          <Text as='h1'>404</Text>
-          <p>
-            {/* @ts-expect-error */}
-            <i>{error?.statusText || error?.message}</i>
-          </p>
-          <Text>Page not found!</Text>
-          <Text>
-            This page is away on a vacation to Hawaii enjoying a coconut drink
-            on a sunny beach so you may not get it today.
-          </Text>
-          <Text>
-            Not to worry. You can go back to the
-            <Link to='/'>Homepage</Link>
-            &nbsp;or another page using the navigation menu
-          </Text>
-          <Text as='strong'>
-            (It will go back to the previous page after 10 seconds)
-          </Text>
-        </div>
-      </div>
+      <ErrorPageTemplate />
     </main>
   );
 };

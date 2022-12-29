4ea7e06a9c40655921da802d1de5a88673de8760
Prince Muel <vansomecsam@gmail.com>
2022-12-29 17:00:43 +0100

    chore: cleanup application entry point
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,12 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Layout } from './components';
+import { RouterProvider } from 'react-router-dom';
 import { CountriesProvider, ThemeProvider } from './context';
-import { Country, countryLoader, ErrorPage, Home } from './routes';
+import { router } from './routes';
 import './styles/main.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-        // loader: homeLoader,
-      },
-      {
-        path: 'countries/:countryId',
-        element: <Country />,
-        loader: countryLoader,
-      },
-    ],
-  },
-]);
 
 root.render(
   <React.StrictMode>

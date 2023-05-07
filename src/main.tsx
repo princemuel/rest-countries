c0a61c4ce8bf2ff31ba2b8f0a31183d0be946a19
Prince Muel <vansomecsam@gmail.com>
2023-05-07 12:10:55 +0100

    chore: cleanup entrypoint file
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
-import { RouterProvider } from 'react-router-dom';
-import { CountriesProvider, ThemeProvider } from './context';
-import { router } from './routes';
-import './styles/main.css';
+import './index.css';
+import { Providers } from './providers';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <ThemeProvider>
-        <CountriesProvider>
-          <RouterProvider router={router} />
-        </CountriesProvider>
-      </ThemeProvider>
-    </HelmetProvider>
+    <Providers />
   </React.StrictMode>
 );

c2d9da03c0e474895d5dffc2e8a36faf6947f62e
Prince Muel <vansomecsam@gmail.com>
2023-05-07 12:24:11 +0100

    feat: update tailwind, vite config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class', '[data-theme="dark"]'],
+  darkMode: ['class', '[data-mode="dark"]'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     borderRadius: {

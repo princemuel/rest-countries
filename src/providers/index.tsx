d04d55aa5d0cad46313ee2ea587044e932162be1
Prince Muel <vansomecsam@gmail.com>
2023-05-07 12:23:20 +0100

    feat: add providers component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/providers/index.tsx
@@ -0,0 +1,36 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import { RouterProvider } from 'react-router-dom';
+import { CountriesProvider, ThemeProvider } from '../lib';
+import { router } from '../routes';
+
+export function Providers() {
+  const [client] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+            staleTime: 1000 * 60 * 60 * 1, // 1 hr
+            retry: 1,
+            refetchOnMount: true,
+            refetchOnReconnect: true,
+            refetchOnWindowFocus: true,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider>
+          <CountriesProvider>
+            <RouterProvider router={router} />
+          </CountriesProvider>
+        </ThemeProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
+  );
+}

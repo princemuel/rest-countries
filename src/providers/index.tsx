438454ca16fe4c5d974efed8c3409d8fc2e03ad1
Prince Muel <vansomecsam@gmail.com>
2023-08-12 04:43:24 +0100

    feat: init query devtools
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,18 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import * as React from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "../lib";
 import { router } from "../routes";
-
 export function Providers() {
   const [client] = React.useState(
     () =>
       new QueryClient({
         defaultOptions: {
           queries: {
-            cacheTime: 1000 * 60 * 60 * 24, // 24 hours
-            staleTime: 1000 * 60 * 60 * 1, // 1 hr
+            cacheTime: 1000 * 60 * 60 * 24 * 2, // 2 days
+            staleTime: 1000 * 60 * 60 * 2, // 2 hrs
             retry: 1,
             refetchOnMount: true,
             refetchOnReconnect: true,
@@ -27,6 +27,7 @@ export function Providers() {
       <HelmetProvider>
         <ThemeProvider>
           <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
         </ThemeProvider>
       </HelmetProvider>
     </QueryClientProvider>

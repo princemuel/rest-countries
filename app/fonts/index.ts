d42ac395297d2d80133af1a8ed51c597d8033e24
Prince Muel <vansomecsam@gmail.com>
2023-08-12 23:28:32 +0100

    feat: add project font
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,7 +1,10 @@
+import { cn } from "@/helpers";
 import { Nunito_Sans } from "next/font/google";
 
 const FontSans = Nunito_Sans({
+  subsets: ["latin"],
+  display: "swap",
   variable: "--font-sans",
 });
 
-export const fonts = FontSans.variable;
+export const fonts = cn(FontSans.variable);

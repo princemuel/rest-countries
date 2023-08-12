342c33990ef6578efcb04ab7b34f56d0a24dd728
Prince Muel <vansomecsam@gmail.com>
2023-08-13 00:33:24 +0100

    feat: setup project helper functions
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ helpers/fetcher.ts
@@ -0,0 +1,35 @@
+export async function request<JSON = any>(
+  ...args: Parameters<typeof fetch>
+): Promise<JSON> {
+  const response = await fetch(...args);
+
+  if (!response.ok) {
+    const json = await response.json();
+
+    let error: FetchError;
+    if (json?.error) {
+      error = new Error(json.error);
+      error.status = response.status;
+    } else {
+      error = new Error('Network response was not ok');
+    }
+
+    // doubly make sure error is not undefined
+    return Promise.reject(
+      error || new Error('An unexpected error occurred. Please try again')
+    );
+  }
+
+  const type = response.headers.get('content-type');
+  if (type !== 'application/json') {
+    return Promise.reject(
+      new TypeError(`Malformed data! Expected 'JSON', got '${type}'`)
+    );
+  }
+
+  return response.json();
+}
+
+interface FetchError extends Error {
+  status?: number;
+}
---
+++ helpers/helpers.ts
@@ -0,0 +1,19 @@
+import { cx } from "cva";
+import type { ClassValue } from "cva/types";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({});
+
+export function cn(...args: ClassValue[]) {
+  return customTwMerge(cx(args));
+}
+
+/*---------------------------------*
+            ARRAY UTILS            *
+  ---------------------------------*
+ */
+export function hasValues<T>(
+  data: T[] | null | undefined
+): data is NonNullable<T[]> {
+  return (data || []).length > 0;
+}

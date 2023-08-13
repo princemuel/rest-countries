614bc26034ebfa4b12f982a3ff6d88712d9b2d4c
Prince Muel <vansomecsam@gmail.com>
2023-08-13 17:27:49 +0100

    feat: add site metadata
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- config/constants.config.ts
+++ config/constants.config.ts
@@ -1,3 +1,5 @@
+export const REST_COUNTRIES_API =
+  process.env.NEXT_PUBLIC_COUNTRIES_BASE_URL || '';
 export const SITE_URL =
   process.env.SITE_URL || process.env.NEXT_PUBLIC_SITE_URL;
 export const baseUrl = new URL('/', SITE_URL);

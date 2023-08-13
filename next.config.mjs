a3bcca965dc9f4136e109d4f6e9ced1971e838f4
Prince Muel <vansomecsam@gmail.com>
2023-08-13 02:30:01 +0100

    ci: update next config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+//@ts-check
+import withPlaiceholder from '@plaiceholder/next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  // typescript: {
+  //   ignoreBuildErrors: process.env.NODE_ENV === 'production',
+  // },
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+    serverActions: true,
+    webVitalsAttribution: ['CLS', 'LCP'],
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+export default withPlaiceholder(nextConfig);

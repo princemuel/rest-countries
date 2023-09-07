145b78f3a3919cdd59749f819e4c66399e09fb4b
Prince Muel <vansomecsam@gmail.com>
2023-09-07 13:04:33 +0100

    feat: update homepage template
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/home.tsx
+++ app/home.tsx
@@ -1,12 +1,21 @@
-import { CountriesList } from '@/components';
+import { Container, CountriesList, Filters } from '@/components';
+import { Suspense } from 'react';
 
 const HomepageTemplate = () => {
   return (
-    <main className='flex flex-col gap-20'>
-      <section className='h-container'></section>
+    <main className='mt-16 flex flex-col gap-20'>
+      <section>
+        <Container>
+          <Filters />
+        </Container>
+      </section>
 
-      <section className='h-container'>
-        <CountriesList />
+      <section>
+        <Container>
+          <Suspense fallback={<div>Loading..</div>}>
+            <CountriesList />
+          </Suspense>
+        </Container>
       </section>
     </main>
   );

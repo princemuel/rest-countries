9cbe5bea9e7cd9c91b2bc5b92b26dabb452418c1
Sam Chukwuzube <vansomecsam@gmail.com>
2024-12-21 01:17:35 +0100

    feat: migrate to astro (#21)
    * chore(ci): setup and validate eslint/prettier/a11y rules
    
    * chore: add server, ci config
    
    * feat: add components
    
    * ci: update lefhook config

--- app/helpers/custom-elements.ts
+++ app/helpers/custom-elements.ts
@@ -1,6 +1,3 @@
-export const create_custom_el = (
-  name: string,
-  ctor: CustomElementConstructor,
-) => {
+export const create_custom_el = (name: string, ctor: CustomElementConstructor) => {
   if (!customElements.get(name)) customElements.define(name, ctor);
 };
